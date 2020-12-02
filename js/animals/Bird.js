00cb9968ebfa4e86e590fb18c5215cc37cfc4a92
anavisnia <73888794+anavisnia@users.noreply.github.com>
2020-12-02 22:51:38 +0200

    Bird described

--- js/animals/Bird.js
+++ js/animals/Bird.js
@@ -1,6 +1,20 @@
 import { Pet } from './Pet.js';
 
 class Bird extends Pet {
+    constructor(name, color, wingsCount) {
+        super(name, color);
+        this.name = name;
+        this.color = color;
+        this.wingsCount = 2;
+    }
+
+    fly() {
+        if (this.wingsCount === 0) {
+            console.log(`${this.name} negali skraidyt, nes neturi sparnu :/.`);
+        } else {
+            console.log(`${this.name} gali skristi turedamas ${this.wingsCount} sparnus.`);
+        }
+    }
 
 }
 
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import { Animal } from './animals/Animal.js';
 import { Dog } from './animals/Dog.js';
 import { Cat } from './animals/Cat.js';
 import { Rabbit } from './animals/Rabbit.js';
+import { Bird } from './animals/Bird.js';
 
 
 
@@ -10,9 +11,15 @@ const rikis = new Dog('Rikis', 'baltos');
 const rainis = new Cat('Rainis', 'juoda');
 const mimi = new Rabbit('Mimi', 'baltos');
 
+const chip = new Bird('Chip', 'rudas', 2);
+
 // rikis.sound();
 // rainis.sound();
 
 rikis.intriduce();
 rainis.intriduce();
 mimi.intriduce();
+
+chip.intriduce();
+chip.fly();
+console.log(chip.wingsCount);
