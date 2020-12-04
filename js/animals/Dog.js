dc2b11d8ee3ae150abd9e51fea7c671774d865eb
anavisnia <73888794+anavisnia@users.noreply.github.com>
2020-12-04 14:28:23 +0200

    constructor update Dog,js

--- js/animals/Dog.js
+++ js/animals/Dog.js
@@ -3,9 +3,6 @@ import { Pet } from './Pet.js';
 class Dog extends Pet {
     constructor(name, color, legCount, likePlaying, voice) {
         super(name, color, legCount, likePlaying, voice);
-        this.name = name;
-        this.color = color;
-        this.legCount = 4;
         this.likePlaying = true;
         this.voice = 'Au au!';
     }
